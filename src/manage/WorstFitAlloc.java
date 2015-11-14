2f90aa97c95cc4bcb140f57fadba070232d19610
navthakur <navthakur@10.188.164.119>
2015-11-13 19:07:30 -0500

    set up worstFitAlloc to work better with BaseManager

--- src/manage/WorstFitAlloc.java
+++ src/manage/WorstFitAlloc.java
@@ -4,51 +4,56 @@ import java.util.Collection;
 
 public class WorstFitAlloc extends BaseManager {
     
-    private MemBlock usedBlocks[];  
-    
+   // private MemBlock usedBlocks[];  
+
     private  PriorityQueue<MemBlock> freeBlocks;
     
-    private int size;
+    //private int size;
     
     private Defrag sortedBlocks;
 
-    public WorstFitAlloc(int memSize) {
+ /*   public WorstFitAlloc(int memSize) {
         
         this.size = memSize;
         this.usedBlocks = new MemBlock[memSize];
         this.freeBlocks = new PriorityQueue<MemBlock>(); 
         this.freeBlocks.add(new MemBlock(0, memSize, true));
     }
-
+*/
+    public WorstFitAlloc(int inMemSize) {
+        super(inMemSize);
+        // TODO Auto-generated constructor stub
+    }
+    
     @Override
     public boolean alloc(int size) {
-        if (size > this.size) {
+        return super.alloc(size);
+     /*   if (size > this.size) {
             return false;
         }
-        MemBlock temp = this.freeBlocks.removeMax();
+      /*  MemBlock temp = this.freeBlocks.getMax();
         if (temp == null) {
             return false;
         }
         if (temp.getSize() < size) {
             sortedBlocks = new Defrag(this.freeBlocks.getList(), size);
             
-        }
-        
-        return false;
+        }*/
     }
 
     @Override
     public boolean dealloc(int id) {
-        
-        return false;
+        return super.dealloc(id);
+        //return false;
     }
 
-    @Override
+ /*   @Override
     public Collection<MemBlock> bucketDefrag() {
-        sortedBlocks = new Defrag(this.freeBlocks.getList(), size);
-        sortedBlocks.bucketSort();
+    /   sortedBlocks = new Defrag(this.freeBlocks.getList(), size);
+        sortedBlocks.bucketSort(); 
         //sorted.
-        return null;
+        return null; 
+
     }
 
     @Override
@@ -57,9 +62,21 @@ public class WorstFitAlloc extends BaseManager {
         sortedBlocks.quickSort();
         return null;
     }
-
+*/
     @Override
     public void rebuild(Collection<MemBlock> blocks) {
+        super.rebuild(blocks);
+        
+    }
+
+    @Override
+    protected MemBlock grabToAlloc(int size) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected void addUnalloc(MemBlock unAlloc) {
         // TODO Auto-generated method stub
         
     }
